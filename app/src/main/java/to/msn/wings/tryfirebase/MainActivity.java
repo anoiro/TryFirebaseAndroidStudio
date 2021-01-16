dbad510192ba5ee8412e5360766571a3de91fede
anoiro <asano.kyoshiro.16@shizuoka.ac.jp>
2021-01-16 14:39:43 +0900

    画面回転しても画面が復元されないようした

--- app/src/main/java/to/msn/wings/tryfirebase/MainActivity.java
+++ app/src/main/java/to/msn/wings/tryfirebase/MainActivity.java
@@ -20,4 +20,21 @@ public class MainActivity extends AppCompatActivity {
         TextView txt = (TextView)findViewById(R.id.txtResult);
         txt.setText(new Date().toString());
     }
+
+    // 画面回転対策
+    // 画面が破棄される前に状態を保存
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        TextView txtResult = (TextView)findViewById(R.id.txtResult);
+        outState.putString("txtResult", txtResult.getText().toString());
+    }
+
+    // 画面が復元される際に状態を取り出し
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView txtResult = (TextView)findViewById(R.id.txtResult);
+        txtResult.setText(savedInstanceState.getString("txtResult"));
+    }
 }
