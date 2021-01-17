0df9baa5b9cf209d809063424ec71717c7a079c7
anoiro <asano.kyoshiro.16@shizuoka.ac.jp>
2021-01-17 19:22:19 +0900

    登録と取得ができるようにした

--- app/src/main/java/to/msn/wings/tryfirebase/MainActivity.java
+++ app/src/main/java/to/msn/wings/tryfirebase/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,10 +14,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.functions.FirebaseFunctions;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,40 +41,59 @@ public class MainActivity extends AppCompatActivity {
 //                this, new Date().toString(), Toast.LENGTH_LONG);
 //        toast.show();
 
-        // ドキュメントとコレクションの指定
-        DocumentReference mDocRef = FirebaseFirestore.getInstance().document("test/sample");
+        // FirestoreのDB
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        // Firebaseへの送信
-        Map<String, Object> data = new HashMap<>();
-        data.put("D","なし");
-        data.put("E","オレンジ");
-        data.put("F","すいか");
-        mDocRef.set(data);
+        // データの登録
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
 
-        // Firebaseからの受信
-        mDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        //値が取得できた時の処理
-                        String save = (String) document.get("E");
-                        Toast toast = Toast.makeText(
-                                MainActivity.this, save, Toast.LENGTH_LONG);
-                        toast.show();
-                    }else{
-                        Toast toast = Toast.makeText(
-                                MainActivity.this, "documentがなかった", Toast.LENGTH_LONG);
-                        toast.show();
+        // Add a new document with a generated ID
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    // 登録成功
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Toast toast = Toast.makeText(
+//                                MainActivity.this, "DocumentSnapshot added with ID: " + documentReference.getId(), Toast.LENGTH_LONG);
+//                        toast.show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    // 登録失敗
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast toast = Toast.makeText(
+//                                MainActivity.this, "失敗した", Toast.LENGTH_LONG);
+//                        toast.show();
+//                    }
+//                });
+
+        // データの取得
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        // 取得成功
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Toast toast = Toast.makeText(
+                                        MainActivity.this, document.getId() + " => " + document.getData(), Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        // 取得失敗
+                        } else {
+                            Toast toast = Toast.makeText(
+                                    MainActivity.this, "Error getting documents.", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
                     }
-                }else{
-                    Toast toast = Toast.makeText(
-                            MainActivity.this, "取得に失敗", Toast.LENGTH_LONG);
-                    toast.show();
-                }
-            }
-        });
+                });
     }
 
     // 画面回転対策
